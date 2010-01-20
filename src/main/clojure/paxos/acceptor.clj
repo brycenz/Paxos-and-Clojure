0d718c53b826425aa3c5d6599d061dc3d5a7de93
Bryce Ewing <brycenz@gmail.com>
2010-01-20 22:13:29 +1300

    Changed acceptor to use references.

--- src/main/clojure/paxos/acceptor.clj
+++ src/main/clojure/paxos/acceptor.clj
@@ -1,30 +1,35 @@
 (ns paxos.acceptor)
 
-(def current-proposal-number 0)
-(def current-proposal-value nil)
+(def current-proposal-number (ref 0))
+(def current-proposal-value (ref nil))
 
 (defn reset-acceptor []
-  (def current-proposal-number 0)
-  (def current-proposal-value nil))
+  (dosync
+    (ref-set current-proposal-number 0)
+    (ref-set current-proposal-value nil)))
 
 (defn proposal-inform [proposal-number proposal-value]
   (list proposal-number proposal-value))
 
 (defn proposal-promise [proposal-number proposal-value]
-  (def current-proposal-number proposal-number)
-  (proposal-inform proposal-number proposal-value))
+  (dosync
+    (alter current-proposal-number (fn [old new] new) proposal-number)
+    (proposal-inform proposal-number proposal-value)))
 
 (defn proposal-accept [proposal-number proposal-value]
-  (def current-proposal-number proposal-number)
-  (def current-proposal-value proposal-value)
-  (proposal-inform proposal-number proposal-value))
+  (dosync
+    (alter current-proposal-number (fn [old new] new) proposal-number)
+    (alter current-proposal-value (fn [old new] new) proposal-value)
+    (proposal-inform proposal-number proposal-value)))
 
 (defn prepare-request [proposal-number]
-  (if (> proposal-number current-proposal-number)
-    (proposal-promise proposal-number current-proposal-value)
-    (proposal-inform current-proposal-number current-proposal-value)))
+  (dosync
+    (if (> proposal-number @current-proposal-number)
+      (proposal-promise proposal-number @current-proposal-value)
+      (proposal-inform @current-proposal-number @current-proposal-value))))
 
 (defn accept-request [proposal-number proposal-value]
-  (if (< proposal-number current-proposal-number)
-    (proposal-inform current-proposal-number current-proposal-value)
-    (proposal-accept proposal-number proposal-value)))
+  (dosync
+    (if (< proposal-number @current-proposal-number)
+      (proposal-inform @current-proposal-number @current-proposal-value)
+      (proposal-accept proposal-number proposal-value))))
